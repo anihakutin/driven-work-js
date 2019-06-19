09b519b41fa89dfd5364a7b23873b79bed0e65a6
Heshie Brody <4hbrody@gmail.com>
2019-06-19 19:46:49 -0400

    Add all routes. Add gihub callback route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions
+  resources :users
+  resources :ceos
+
+  resources :companies do
+    resources :problems
+    resources :technology_breakthroughs
+  end
+
+  get '/auth/github/callback', to: 'sessions#create'
+
 end
