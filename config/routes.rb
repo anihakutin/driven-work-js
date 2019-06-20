d9b43d6e2a7190a21d6e33f992f66135c7b05548
Heshie Brody <4hbrody@gmail.com>
2019-06-20 18:50:11 -0400

    Add user/login route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'companies#index'
 
+  get '/users/login', to: 'users#new'
+  
   resources :sessions
   resources :users
   resources :ceos
