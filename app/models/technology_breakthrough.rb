aa063658a0de0f879494145725fdada072f596e3
Heshie Brody <4hbrody@gmail.com>
2019-06-20 18:30:34 -0400

    Add methods: Company name, Years Solved, Solved?.

--- app/models/technology_breakthrough.rb
+++ app/models/technology_breakthrough.rb
@@ -1,3 +1,15 @@
 class TechnologyBreakthrough < ApplicationRecordx
   belongs_to :company
+
+  def company_name
+    self.company.name
+  end
+
+  def years_ago_solved
+    self.solved? ? DateTime.now.year - self.year : "Not Solved Yet"
+  end
+
+  def solved?
+    self.year != 0
+  end
 end
