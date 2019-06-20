0b2bd08ced7cf78aade973b1fef5869ec9ee0c83
Heshie Brody <4hbrody@gmail.com>
2019-06-20 16:33:38 -0400

    Updated seeds to reflect date field types.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,15 @@ DATA = {
   :user_keys =>
     ["name", "email", "password_digest", "admin"],
   :users => [
-    ["Max Charles", "#{rand(1000..5000)}@gmail.com" "password", true],
-    ["Skai Jackson", "#{rand(1000..5000)}@gmail.com" "password", false],
-    ["Kaleo Elam", "#{rand(1000..5000)}@gmail.com" "password", false],
-    ["Megan Charpentier", "#{rand(1000..5000)}@gmail.com" "password", false],
-    ["Hayden Byerly", "#{rand(1000..5000)}@gmail.com" "password", false],
-    ["Tenzing Norgay Trainor", "#{rand(1000..5000)}@gmail.com" "password", false],
-    ["Davis Cleveland","#{rand(1000..5000)}@gmail.com" "password", false],
-    ["Cole Sand","#{rand(1000..5000)}@gmail.com" "password", true],
-    ["Quvenzhané Wallis", "#{rand(1000..5000)}@gmail.com" "password", true]
+    ["Max Charles", "#{rand(1000..5000)}@gmail.com", "password", true],
+    ["Skai Jackson", "#{rand(1000..5000)}@gmail.com", "password", false],
+    ["Kaleo Elam", "#{rand(1000..5000)}@gmail.com", "password", false],
+    ["Megan Charpentier", "#{rand(1000..5000)}@gmail.com", "password", false],
+    ["Hayden Byerly", "#{rand(1000..5000)}@gmail.com", "password", false],
+    ["Tenzing Norgay Trainor", "#{rand(1000..5000)}@gmail.com", "password", false],
+    ["Davis Cleveland", "#{rand(1000..5000)}@gmail.com", "password", false],
+    ["Cole Sand", "#{rand(1000..5000)}@gmail.com", "password", true],
+    ["Quvenzhané Wallis", "#{rand(1000..5000)}@gmail.com", "password", true]
   ],
   :company_keys =>
     ["name", "description", "year_founded", "user_id", "ceo_id"],
@@ -30,21 +30,21 @@ DATA = {
   :ceo_keys =>
     ["name", "year_born", "net_worth"],
   :ceos => [
-    ["Elon Musk", "6-28-1971", 18900000000],
-    ["Clark Valberg", "6-28-1978", 500000000],
-    ["Steve Jobs", "2-24-1955", 19000000000]
+    ["Elon Musk", "1971", 18900000000],
+    ["Clark Valberg", "1978", 500000000],
+    ["Steve Jobs", "1955", 19000000000]
   ],
   :problem_keys =>
     ["name", "description", "year_solved", "company_id"],
   :problems => [
-    ["Cars That Run On Sustainable Energy", "Create compelling electric vehicles for an affordable price", "2-01-2008", 1],
-    ["Get design feedback from non technical people ", "Create a platform which allows creators to get feedback from non-technical people, clients and alike", "1-01-2011", 2]
+    ["Cars That Run On Sustainable Energy", "Create compelling electric vehicles for an affordable price", "2008", 1],
+    ["Get design feedback from non technical people ", "Create a platform which allows creators to get feedback from non-technical people, clients and alike", "2011", 2]
   ],
   :technology_keys =>
     ["name", "description", "year", "company_id"],
   :technology_breakthroughs => [
-    ["Affordable and reliable electric battery", "Created affordable and reliable electric batteries that can be used in cars", "2-01-2008", 1],
-    ["Self landing Rockets", "Made rocket travel cheaper by landing them and recycling them fro future flights.", "3-30-2017", 3]
+    ["Affordable and reliable electric battery", "Created affordable and reliable electric batteries that can be used in cars", "2008", 1],
+    ["Self landing Rockets", "Made rocket travel cheaper by landing them and recycling them fro future flights.", "2017", 3]
   ]
 }
 
