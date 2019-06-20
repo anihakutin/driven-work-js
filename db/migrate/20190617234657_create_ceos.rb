bb586eee8ce8fb86cf60b82ce522a78d5e380dd3
Heshie Brody <4hbrody@gmail.com>
2019-06-19 21:21:58 -0400

    Changed :year_born to date type.

--- db/migrate/20190617234657_create_ceos.rb
+++ db/migrate/20190617234657_create_ceos.rb
@@ -2,7 +2,7 @@ class CreateCeos < ActiveRecord::Migration[5.2]
   def change
     create_table :ceos do |t|
       t.string :name
-      t.integer :year_born
+      t.date :year_born
       t.decimal :net_worth
 
       t.timestamps null: false
