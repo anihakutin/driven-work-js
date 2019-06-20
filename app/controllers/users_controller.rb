1c7b12188f8d4fb9655bcbd7e1cc670312eec322
Heshie Brody <4hbrody@gmail.com>
2019-06-20 18:48:43 -0400

    Add :UserController, #new #create #show.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,26 @@
 class UsersController < ApplicationController
-  def index
-
-  end
-
   def new
-
+    @user = User.new
+    render 'users/login'
   end
 
   def create
-
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to new_user_path
+    end
   end
 
   def show
-
+    user = User.find(params[:id])
+    if user == current_user
+      @user = User.find(params[:id])
+    else
+      redirect_to root_path
+    end
   end
 
   def edit
@@ -26,4 +34,9 @@ class UsersController < ApplicationController
   def destroy
 
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password_digest, :admin)
+    end
 end
