dcc7e6aa8cc966014c473dce1c0d80d21ee3131b
Heshie Brody <4hbrody@gmail.com>
2019-06-20 18:27:48 -0400

    Add methods: Name, Solved?.

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,3 +1,15 @@
 class Problem < ApplicationRecord
   belongs_to :company
+
+  def company_name
+    self.company.name
+  end
+
+  def years_ago_solved
+    self.solved? ? DateTime.now.year - self.year_solved : "Not Solved Yet"
+  end
+
+  def solved?
+    self.year_solved != 0
+  end
 end
