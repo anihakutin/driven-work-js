2f3a9502c61841af1dc05397a34b9035e9877182
Heshie Brody <4hbrody@gmail.com>
2019-06-19 20:56:55 -0400

    Add company methods.

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,22 @@ class Company < ActiveRecord::Base
   has_many :technology_breakthroughs
   belongs_to :user
   belongs_to :ceo
+
+  def technology_breakthroughs_count
+    technology_breakthroughs.count
+  end
+
+  def problems_solved
+    problems.where('year_solved != ?', 0).count
+  end
+
+  def age
+    age_in_days = DateTime.now.to_date - self.year_founded
+    years = age_in_days.to_i / 365
+  end
+
+  def ceo_name
+    self.ceo.name
+  end
+
 end
