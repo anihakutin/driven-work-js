f26aa9e12239cf494d0bd8daa39ac70a4bdb8905
Heshie Brody <4hbrody@gmail.com>
2019-06-20 21:32:58 -0400

    Check if github user is valid.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,12 @@ class SessionsController < ApplicationController
   def create
     if auth.present?
       user = User.find_or_create_by_omniauth(auth["info"])
-      session[:user_id] = user.id
-      @user = current_user
-      redirect_to user_path(@user)
+      if user.valid?
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        redirect_to root_path
+      end
     else
       user = User.find_by(email: user_params[:email])
       if user && user.authenticate(user_params[:password])
@@ -20,6 +23,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.clear
+    redirect_to root_path
   end
 
   private
