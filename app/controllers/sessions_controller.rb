992d64e63638ddc5a0a9fe9f4941740f4fcaeb5d
Heshie Brody <4hbrody@gmail.com>
2019-06-20 20:07:36 -0400

    Add github, local login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,21 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_or_create_by_omniauth(auth["info"])
-    session[:user_id] = user.id
-    @auth = auth
+    if auth.present?
+      user = User.find_or_create_by_omniauth(auth["info"])
+      session[:user_id] = user.id
+      @user = current_user
+      redirect_to user_path(@user)
+    else
+      user = User.find_by(email: user_params[:email])
+      if user && user.authenticate(user_params[:password])
+        session[:user_id] = user.id
+        @user = current_user
+        redirect_to user_path(@user)
+      else
+        flash[:alert] = "Login Information Incorrect"
+        redirect_to root_path
+      end
+    end
   end
 
   def destroy
@@ -12,5 +25,9 @@ class SessionsController < ApplicationController
   private
     def auth
       request.env['omniauth.auth']
+    end
+
+    def user_params
+      params.require(:session).permit(:email, :password)
     end
 end
