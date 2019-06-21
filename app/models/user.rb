41f64da436138b804b37338109a954cdd9eb86ef
Heshie Brody <4hbrody@gmail.com>
2019-06-20 20:08:01 -0400

    Add #has_secure_password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :companies
+
 
   def self.find_or_create_by_omniauth(auth_hash)
     self.where(email: auth_hash["email"]).first_or_create do |user|
