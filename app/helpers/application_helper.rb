cbe31097e70b1beedab5f73403998edfb7f89bf9
Heshie Brody <4hbrody@gmail.com>
2019-06-24 20:39:29 -0400

    Fix problem link description spelling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,7 +33,7 @@ module ApplicationHelper
 
   def problems_solved_link(company)
     if company.problems.empty?
-      link_to("Add Problems", new_company_problem_path(company))
+      link_to("Add Problem", new_company_problem_path(company))
     else
        company.problems_count
      end
