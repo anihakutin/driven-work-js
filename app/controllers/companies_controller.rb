c5a3bdf26ddbb5680e6096461f7519ade002af33
Heshie Brody <4hbrody@gmail.com>
2019-06-24 20:05:29 -0400

    Add #before_action, #require_login. Update actions, fix #user_params

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,22 +1,30 @@
 class CompaniesController < ApplicationController
-  def index
+  before_action :require_login
+  skip_before_action :require_login, only: [:index]
 
+  def index
+    @companies = Company.all
   end
 
   def new
-
+      @company = current_user.companies.build
   end
 
   def create
-
+    @company = Company.new(user_params)
+    if @company.save
+      redirect_to company_path(@company)
+    else
+      render :new
+    end
   end
 
   def show
-
+    @company = current_user.companies.find_by(id: params[:id]) or render_404
   end
 
   def edit
-
+    @company = current_user.companies.find_by(id: params[:id]) or render_404
   end
 
   def update
@@ -26,4 +34,9 @@ class CompaniesController < ApplicationController
   def destroy
 
   end
+
+  private
+    def user_params
+      params.require(:company).permit(:name, :description, :year_founded, :user_id, :ceo_id)
+    end
 end
