50be62ae7d98e42cd558d9d2f8e294e44f832125
Heshie Brody <4hbrody@gmail.com>
2019-06-24 20:25:25 -0400

    Add #problems_solved_link.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,5 +31,13 @@ module ApplicationHelper
      end
   end
 
+  def problems_solved_link(company)
+    if company.problems.empty?
+      link_to("Add TB", new_company_problem_path(company))
+    else
+       company.problems_count
+     end
+  end
+
 
 end
