caee982ceafe3a9ae4f552462f314435888e1bcc
Heshie Brody <4hbrody@gmail.com>
2019-06-27 19:49:00 -0400

    Reorder some method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   require "base64"
-  has_secure_password
   has_many :companies
+  has_secure_password
 
+  
 
   def self.find_or_create_by_omniauth(auth_hash)
     self.where(email: auth_hash["email"]).first_or_create do |user|
