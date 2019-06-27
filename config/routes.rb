fef0b97d1bd65977786dd5411c725026f4736eae
Heshie Brody <4hbrody@gmail.com>
2019-06-26 20:46:07 -0400

    Change routing order cause why not...?

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'companies#index'
 
-  get '/users/login', to: 'users#new'
   get '/login', to: 'users#new'
+  get '/users/login', to: 'users#new'
+
   post   '/login',   to: 'sessions#create'
   post '/logout',  to: 'sessions#destroy'
 
