2cd282b27b1b0047d7d1803439faef1b3a9a31a2
Heshie Brody <4hbrody@gmail.com>
2019-06-27 19:44:46 -0400

    Add validations.

--- app/models/ceo.rb
+++ app/models/ceo.rb
@@ -3,6 +3,11 @@ class Ceo < ApplicationRecord
   has_many :problems, through: :companies
   has_many :technology_breakthroughs, through: :companies
 
+  validates :name, :year_born, :net_worth, presence: true
+  validates :year_born, numericality: { only_integer: true }
+  validates :year_born, length: { is: 4 }
+  validates :net_worth, numericality: { only_integer: true }
+
   def technology_breakthroughs
     technology_breakthroughs.count
   end
