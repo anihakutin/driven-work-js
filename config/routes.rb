3121cbd7b035b22f037558078d64819064b78179
Heshie Brody <4hbrody@gmail.com>
2019-06-30 15:32:06 -0400

    Remove unnecessary routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'companies#index'
 
-  get '/login', to: 'users#new'
-  get '/users/login', to: 'users#new'
-
   post   '/login',   to: 'sessions#create'
   post '/logout',  to: 'sessions#destroy'
 
