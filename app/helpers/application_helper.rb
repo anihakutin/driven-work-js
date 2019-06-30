fe69261c47d8ff995b817ce109d37879326e9693
Heshie Brody <4hbrody@gmail.com>
2019-06-30 15:05:07 -0400

    Refactor user show page with helper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,10 +40,29 @@ module ApplicationHelper
   end
 
   def display_errors(object)
+    errors = []
+    errors << flash[:alert] if flash.any?
+
     if object.errors.any?
       object.errors.full_messages.each do |msg|
-        content_tag(:div, content_tag(:li, msg ), class: "error_explanation")
+        errors << " #{msg}"
       end
     end
+
+    errors.join(",")
+  end
+
+  def current_user_level(user)
+    "ADMIN" if user.admin?
+
+    "Just Another Human..."
+  end
+
+  def current_user_edit_link(user)
+    link_to("Edit Profile", edit_user_path) if user == current_user
+  end
+
+  def current_user_delete_link(user)
+    button_to("Delete Profile", @user, :method => :delete) if user == current_user
   end
 end
