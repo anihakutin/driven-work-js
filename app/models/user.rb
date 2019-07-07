2deb98b3b4ebd6252e076f5d60de807f4c299fd5
Heshie Brody <4hbrody@gmail.com>
2019-07-07 14:59:01 -0400

    Add random pass for gihub auth, has many ceos, remove pass validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,16 @@ class User < ApplicationRecord
   require "base64"
   has_many :companies
   has_secure_password
+  has_many :ceos, through: :companies
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
-  validates :password_digest, presence: { message: "Password can't be blank"}
 
   def self.find_or_create_by_omniauth(auth_hash)
     self.where(email: auth_hash["email"]).first_or_create do |user|
       user.email = auth_hash["email"]
       user.name = auth_hash["name"]
-      user.password = "passWord" # fixme
+      user.password = SecureRandom.hex(13)
     end
   end
 end
