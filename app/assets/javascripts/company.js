c35816be473a7c27b4298e1bac2b72b88d185b52
Heshie Brody <4hbrody@gmail.com>
2019-10-28 00:22:08 -0400

    Update company js class and listItem method to handle more properties

--- app/assets/javascripts/company.js
+++ app/assets/javascripts/company.js
@@ -1,12 +1,15 @@
 class Company {
-  constructor(id, name, description, authorId) {
+  constructor(id, name, description, authorId,  authorName, numOfBreakthroughs) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.authorId = authorId;
+    this.authorName = authorName;
+    this.numOfBreakthroughs = numOfBreakthroughs;
   }
 
   listItem () {
-    return `<li> <a href=/companies/${this.id}>${this.name}</a></li>`
+    return `<li> <a href=/companies/${this.id}>${this.name} - Major Breakthroughs: ${this.numOfBreakthroughs}</a></li>
+            <p>by <a href=/users/${this.authorId}>${this.authorName}</a></p>`
   }
 }
