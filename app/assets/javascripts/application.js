0ac5fc2ba3acf3ba3adb54d15f2446e29c2dc9dc
Heshie Brody <4hbrody@gmail.com>
2019-11-03 01:10:56 -0500

    Remove turbo links to fix ajax cache issues.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,8 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
 //= require jquery
+//= require_tree .
 //= require bootstrap-sprockets
 //= require bootstrap/scrollspy
 //= require bootstrap/modal
